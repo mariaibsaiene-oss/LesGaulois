90928e1e13dd4550d8057f09a227bab32d8ae5e9
MariaIBSAIENE <nm_ibsaiene@esi.dz>
2025-11-02 23:19:17 +0100

    invariant pre post condition + les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,13 +1,23 @@
 package personnages;
+import objets.Equipement;
 
 public class Romain {
 	private String nom;
 	private int force;
 	
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
+	
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified() ;
 	}
+	
+	private boolean isInvariantVerified() {
+		return this.force>= 0;
+	}
+	
 	public String getNom() {
 		return nom;
 	}
@@ -25,11 +35,52 @@ public class Romain {
 		this.force = force;
 	}
 	public void recevoirCoup(int forceCoup) {
-		force -= forceCoup;
-		if (force > 0) {
-			System.out.println("Aie");
+		assert forceCoup > 0 ;
+		
+		int forceAvant = this.force;
+		
+		this.force -= forceCoup;
+		if (this.force < 1) {
+			parler("J'abandonne !");
 		} else {
-			System.out.println("J'abandonne");
+            parler("Aïe");
 		}
+		
+		assert this.force <= forceAvant; //postcondition 
+		assert isInvariantVerified();
+	}
+	
+	public void sEquiper(Equipement equipement) {
+        switch (nbEquipement) {
+        case 2:
+        	System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+            break;
+        case 1:
+        	if (equipements[0] == equipement) {
+                System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+            } else {
+                addEquipement(equipement);
+            }
+            break;
+        case 0 : 
+        	addEquipement(equipement);
+            break;
+        }
+	}
+	
+	private void addEquipement(Equipement e) {
+        equipements[nbEquipement++] = e;
+        System.out.println("Le soldat " + nom + " s'équipe avec un " + e + ".");
+    }
+	
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+		
+		Romain r = new Romain("Minus", 6);
+        r.sEquiper(Equipement.CASQUE);
+        r.sEquiper(Equipement.CASQUE);
+        r.sEquiper(Equipement.BOUCLIER);
+        r.sEquiper(Equipement.CASQUE);
 	}
 }
